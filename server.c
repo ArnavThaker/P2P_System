887aa3d7576f4f7c5dec2bf2fd83ca8cc6f96e13
Arnav Thaker <arnav.thaker@gmail.com>
2022-02-28 14:35:00 -0800

    Added headers and dependent libraries to server file.

---
+++ server.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys.socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <err.h>
+#include <arpa/inet.h>
+
+#include "server.h"
+
+/*
+
+ */
+bool connected(const char* ip, uint16_t port) {
+  
+}
