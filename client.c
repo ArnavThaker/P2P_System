e310a6acab4e853c16a054d422463ce42002ba44
Arnav Thaker <arnav.thaker@gmail.com>
2022-03-03 01:16:35 -0800

    Minor semantic changes to client.c and complete overhaul of file data structure in data_structrs.c

--- client.c
+++ client.c
@@ -16,26 +16,29 @@
 #include "client.h"
 #define PORT 5000
 #define IP "127.0.0.1"
-
+#define FILE_SIZE 1024
+#define MAX_CLIENTS 10
+#define MAX_FILES 5
 
 int cli_sd = -1;
-
+int files[
 /*
 Need method for socket connection
  */
+
 bool connected(const char *ip, uint16_t port) {
   cli_sd = socket(AF_INET, SOCK_STREAM, 0);
 
-  struct sockaddr_in binAddr;
+  struct sockaddr_in client;
 
-  binAddr.sin_family = AF_INET;
-  binAddr.sin_port = htons(PORT);
+  client.sin_family = AF_INET;
+  client.sin_port = htons(PORT);
 
-  if (inet_aton(IP, &binAddr.sin_addr) == 0) {
+  if (inet_aton(IP, &client.sin_addr) == 0) {
     return false;
   } else if (cli_sd == -1) {
     return false;
-  } else if (connect(cli_sd, (const struct sockaddr *) &binAddr, sizeof(binAddr)) == -1) {
+  } else if (connect(cli_sd, (const struct sockaddr *) &client, sizeof(client)) == -1) {
     return false;
   } else {
     return true;
--- data_structs.c
+++ data_structs.c
@@ -1,32 +1,16 @@
 #include "data_structs.h"
 
-list_t* create_list(size_t len) {
-  list_t* list = (list_t*) malloc(len * sizeof(file_t));
+#define MAX_FILE_SIZE 1024
+#define CHUNKS 4
 
-  if (list == NULL) {
-    return -1;
+typedef struct {
+  char[50] name;
+  char[MAX_FILE_SIZE] data;
+  int lenth; 
+} sim_file;
+
+void divide_file (sim_file file) {
+  for (int i = 0; i < MAX_FILE_SIZE; i++) {
+    
   }
-  
-  list->head_file->next = NULL;
-  list->head_file->size = FILE_SIZE;
-  return list;
-}
-
-void destroy_list(list* list) {
-  if (list == NULL) {
-    return;
-  } else {
-    list->next = NULL;
-    free(list);
-  }
-}
-
-file_t* get_next(list_t* list) {
-  
-}
-
-file_t* get_prev(list_t* list) {
-}
-
-void create_chunks(file_t* file) {
 }
